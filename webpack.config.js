713dcd5e6458e3fcb948c18278b8981f4e6fe10c
Claudio Zurita <claudiozurita87@gmail.com>
2017-12-28 13:39:04 -0300

    Se agregó hash a archivos js

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js'/*,
+    filename: '[name].[hash].js'/*,
     publicPath: './dist'*/
   },
   module: {
